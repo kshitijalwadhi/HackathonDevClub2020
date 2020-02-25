bd8072c2da56e825f06bff05ab92121e20147cf2
Kshitij Alwadhi <kshitijalwadhi@gmail.com>
2020-02-25 13:15:52 +0530

    Update aqi.js

--- assets/js/aqi.js
+++ assets/js/aqi.js
@@ -1,36 +1,29 @@
-var city = document.querySelector('#atime_div');
-var time = document.querySelector('.aist_div');
-var icon = document.querySelector('#icon_div')
-
-
+var city = document.querySelector("#atime_div");
+var time = document.querySelector(".aist_div");
+var icon = document.querySelector("#icon_div");
 
 function getWeather() {
-  let api = 'https:api.airvisual.com/v2/nearest_city?lat=48.02&lon=-50.20&key=ee08e794-47c5-41d4-b543-da237c526ffa';
+  let api =
+    "https:api.airvisual.com/v2/nearest_city?lat=28.58&lon=77.23&key=71e51ad9-c732-4497-b57e-662911438596";
 
   fetch(api)
-    .then(function (response) {
+    .then(function(response) {
       let data = response.json();
       return data;
       console.log(data);
     })
-    .then(function (data) {
-      var tempCity = data['data']['current']['pollution']['aqius'];
-      var tempTime = data['data']['current']['weather']['ic'];
+    .then(function(data) {
+      var tempCity = data["data"]["current"]["pollution"]["aqius"];
+      var tempTime = data["data"]["current"]["weather"]["ic"];
 
-
-      atime_div.innerHTML = "The Current Air Quality Index (by US AQI Standards) is " + tempCity;
-
+      atime_div.innerHTML =
+        "The Current Air Quality Index (by US AQI Standards) is " + tempCity;
 
       var img = document.createElement("img");
       img.src = "https://www.airvisual.com/images/" + tempTime + ".png";
       img.height = 50;
       var src = document.getElementById("aist_div");
       src.appendChild(img);
-
-    })
-
-
-
-
+    });
 }
 getWeather();
